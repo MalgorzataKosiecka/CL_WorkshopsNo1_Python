93bd8647ab109bd6d669fe5db15924ce4e16f34c
Małgorzata Kosiecka <malgorzata.kosiecka@gmail.com>
2018-01-13 21:35:13 +0100

    numbers game 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Warsztaty_1
+#####
+Workshops No. 1
+Programs written for workshops during programming course at Coders Lab.
+
+#####
+numbers_game_1.py
+Simple game with computer. The computer draws a number in range 1 to 100. The user should guess the number.
+
---
+++ numbers_game_1.py
@@ -0,0 +1,28 @@
+from random import randint
+import time
+
+guessed = False
+computer_number = randint(1, 100)
+
+print("The computer draws a number from 1 to 100 range. Try to guess the number.")
+time.sleep(2)
+
+while not guessed:
+    user_number_str = input("Guess the number: ")
+
+    try:
+        user_number_int = int(user_number_str)
+    except ValueError:
+        print("This is not a number.")
+        continue
+
+    if user_number_int == computer_number:
+        print("You won!")
+        guessed = True
+    elif user_number_int < computer_number:
+        print("Not enough!")
+        continue
+    else:
+        print("Too much!")
+        continue
+
